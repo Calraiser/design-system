d00e424d1285b5a9a4e00cd590abb947967fa973
cae33 <iapuser@mbp-de-iapuser.hi.inet>
2018-10-03 18:03:08 +0200

    Helpers NavTree

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 
 require 'lib/middleman-navtree/helpers'
-include NavTree
+helpers NavTree
 
 set :markdown_enginge, :redcarpet
 set :markdown, fenced_code_blocks: true
@@ -52,6 +52,35 @@ end
 
 # Helpers
 helpers do
+
+  def previous_link(sourcetree)
+    pagelist = flatten_source_tree(sourcetree)
+    position = get_current_position_in_page_list(pagelist)
+    # Skip link generation if position is nil (meaning, the current page isn't in our
+    # pagination pagelist).
+    if position
+      prev_page = pagelist[position - 1]
+      options = {:class => "previous"}
+      unless first_page?(pagelist)
+        link_to(File.basename(prev_page,File.extname(prev_page)).split('_'), prev_page, options)
+      end
+    end
+  end
+
+  def next_link(sourcetree)
+    pagelist = flatten_source_tree(sourcetree)
+    position = get_current_position_in_page_list(pagelist)
+    # Skip link generation if position is nil (meaning, the current page isn't in our
+    # pagination pagelist).
+    if position
+      next_page = pagelist[position + 1]
+      options = {:class => "next"}
+      unless last_page?(pagelist)
+        link_to(File.basename(next_page,File.extname(next_page)).split('_'), next_page, options)
+      end
+    end
+  end
+
   def nav_link(name, url, id, options={})
     options = {
       class: "",
