4b5acd8b83afd2108278f43ae031c7aa575aaf40
carlos.alvarez <carlos.alvarez@28APO5491.local>
2019-01-23 13:36:22 +0100

    JS Animation SVG

--- source/assets/javascripts/components/onBoarding.js
+++ source/assets/javascripts/components/onBoarding.js
@@ -2,6 +2,7 @@
 var Modal = CarbonComponents.Modal;
 var element = Modal.create(document.getElementById('onBoarding'));
 var slider = $('.onBoarding-slide');
+var slider_svg = $('.onBoarding-slide__figure svg');
 
 
 $(document).ready(function() {
@@ -11,12 +12,51 @@ $(document).ready(function() {
     Cookies("popup_1_2", "2");
   }
 
+
   slider.slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: false,
+    prevArrow: false,
+    nextArrow: $('.next')
   });
+
+
+  var currSlide = 0;
+  var nextSlide = 0;
+
+
+  slider.on('beforeChange', function(event, slick, currentSlide){
+    if (currentSlide === 0) {
+        $('.slick-slide').find(slider_svg).fadeIn('slow');
+    } else {
+        $('.slick-slide').find(slider_svg).fadeOut('slow');
+    }
+});
+
+  function applyHiddenClass() {
+
+    $.each($('.slick-slide'), function() {
+      if ($(this).attr('aria-hidden') == 'true') {
+        $(this).find(slider_svg).fadeOut('slow');
+      } else {
+        $(this).find(slider_svg).fadeIn('slow');
+      }
+    });
+  }
+
+  slider.on('setPosition', function(event, slick, currentSlide) {
+    applyHiddenClass();
+  });
+
+  slider.on('beforeChange', function(event, slick, currentSlide) {
+    currSlide = currentSlide;
+  });
+
+  if (element) {
+    applyHiddenClass();
+  }
 
 });
